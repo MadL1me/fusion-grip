e16c9adb674b9019e589219e072d64eb57209b34
Ilya Klimenko <sn0wy-0wl@yandex.ru>
2024-01-12 15:28:21 +0300

    upd meta plugin information

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     id("org.jetbrains.intellij") version "1.15.0"
 }
 
-group = "madlime"
-version = "1.0-SNAPSHOT"
+group = "com.madlime"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
